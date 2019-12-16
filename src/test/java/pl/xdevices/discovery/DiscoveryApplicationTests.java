db408d5c854beae25ff17a9e31fabf5ac9a56fa1
rfashwal <rfashwal@hotmail.com>
2019-12-16 18:31:08 +0200

    discovery service

---
+++ src/test/java/pl/xdevices/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.xdevices.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
